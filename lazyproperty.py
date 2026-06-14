42b6875d8d23ac83d2540f3ee2500bcb6e4ad48f
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2026-06-14 21:13:35 +0300

    Initial import
    Add lazyproperty, Circle classes

---
+++ lazyproperty.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# PYTHON_ARGCOMPLETE_OK
+import math
+
+
+class lazyproperty:
+    def __init__(self, func):
+        self.func = func
+        self.cache = func.__name__ + "_cache"
+
+    def __get__(self, instance, owner=None):
+        if instance is None:
+            return self
+        if hasattr(instance, self.cache):
+            return getattr(instance, self.cache)
+        res = self.func()
+        setattr(instance, self.cache, res)
+        return res
+
+
+class Circle:
+    def __init__(self, radius):
+        self.radius = radius
+
+    @lazyproperty
+    def area(self):
+        print("Calculating area")
+        return math.pi * self.radius**2
+
+    @lazyproperty
+    def circumference(self):
+        print("Calculating circumference")
+        return 2 * math.pi * self.radius
+
+
+if __name__ == "__main__":
+    c = Circle(12.3)
+    print(c.vars())
+    print(c.area)
+    print(c.vars())
+    print(c.circumference)
+    print(c.area)
